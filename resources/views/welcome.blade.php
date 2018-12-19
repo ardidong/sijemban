940445ccfa8b521038f603d075f1be8b87bcc3b7
Ardian Dwi Rifai <ardimail101@gmail.com>
2018-12-19 09:45:52 +0700

    dropdown dashboard

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,6 +81,11 @@
                   </a>
 
                   <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    @if(Auth::user()->hasRole('admin'))   
+                        <a class="dropdown-item" href="{{route('bencana')}}">Dashboard</a>
+                    @elseif(Auth::user()->hasRole('petugas'))
+                        <a class="dropdown-item" href="{{route('jemput.index')}}">Dashboard</a>
+                    @endif
                       <a class="dropdown-item " href="{{ route('logout') }}"
                           onclick="event.preventDefault();
                                         document.getElementById('logout-form').submit();">
