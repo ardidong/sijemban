66cd1d45a7cc399248979e119c9ac1b1e217b142
Ardian Dwi Rifai <ardimail101@gmail.com>
2019-07-04 17:41:22 +0700

    ganti cara query jumlah kecamatan + fix id query donator

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -35,7 +35,7 @@ class AdminController extends Controller
        $hitung_donatur = array();
        for ($i=0; $i < 12 ; $i++) {
          $hitung_donatur[$i] = DB::table('users')->join('role_user', function($join) {
-                                                                      $join->on('users.id','=','role_user.user_id')->where('role_user.role_id','=',3);
+                                                                      $join->on('users.id','=','role_user.user_id')->where('role_user.role_id','=',1);
                                                                     })->whereMonth('created_at','=',$i+1)->count();
        }
       return view('admin',compact('tanggal'))->with(['hitung_dijemput'=>$hitung_dijemput, 'hitung_diajukan'=>$hitung_diajukan, 'hitung_donatur'=>$hitung_donatur])
@@ -55,42 +55,50 @@ class AdminController extends Controller
   }
 
   public function hitungLokasi(){
-      $hasil = array();
-
-      $hasil[0] = Arr::add(['name' => 'Berbah'], 'value', Donasi::where('kecamatan','like','%Berbah%')
-      ->count());
-      $hasil[1] = Arr::add(['name' => 'Cangkringan'], 'value', Donasi::where('kecamatan','like','%Cangkringan%')
-      ->count());
-      $hasil[2] = Arr::add(['name' => 'Depok'], 'value', Donasi::where('kecamatan','like','%Depok%')
-      ->count());
-      $hasil[3] = Arr::add(['name' => 'Gamping'], 'value', Donasi::where('kecamatan','like','%Gamping%')
-      ->count());
-      $hasil[4] = Arr::add(['name' => 'Godean'], 'value', Donasi::where('kecamatan','like','%Godean%')
-      ->count());
-      $hasil[5] = Arr::add(['name' => 'Kalasan'], 'value', Donasi::where('kecamatan','like','%Kalasan%')
-      ->count());
-      $hasil[6] = Arr::add(['name' => 'Minggir'], 'value', Donasi::where('kecamatan','like','%Minggir%')
-      ->count());
-      $hasil[7] = Arr::add(['name' => 'Mlati'], 'value', Donasi::where('kecamatan','like','%mlati%')
-      ->count());
-      $hasil[8] = Arr::add(['name' => 'Moyudan'], 'value', Donasi::where('kecamatan','like','%Moyudan%')
-      ->count());
-      $hasil[9] = Arr::add(['name' => 'Ngaglik'], 'value', Donasi::where('kecamatan','like','%Ngaglik%')
-      ->count());
-      $hasil[10] = Arr::add(['name' => 'Ngemplak'], 'value', Donasi::where('kecamatan','like','%Ngemplak%')
-      ->count());
-      $hasil[11] = Arr::add(['name' => 'Pakem'], 'value', Donasi::where('kecamatan','like','%Pakem%')
-      ->count());
-      $hasil[12] = Arr::add(['name' => 'Prambanan'], 'value', Donasi::where('kecamatan','like','%Prambanan%')
-      ->count());
-      $hasil[13] = Arr::add(['name' => 'Seyegan'], 'value', Donasi::where('kecamatan','like','%Seyegan%')
-      ->count());
-      $hasil[14] = Arr::add(['name' => 'Sleman'], 'value', Donasi::where('kecamatan','like','%Sleman%')
-      ->count());
-      $hasil[15] = Arr::add(['name' => 'Tempel'], 'value', Donasi::where('kecamatan','like','%Tempel%')
-      ->count());
-      $hasil[16] = Arr::add(['name' => 'Turi'], 'value', Donasi::where('kecamatan','like','%Turi%')
-      ->count());
+      $hasil = array(); 
+      $tes = DB::table('donasis')
+                    ->select(DB::raw(' kecamatan , count(kecamatan) as jumlah'))
+                    ->groupBy('kecamatan')
+                    ->get();
+      
+      foreach($tes as $lokasi){
+        $hasil[] = Arr::add(['name' => $lokasi->kecamatan],'value', $lokasi->jumlah );  
+      } 
+ 
+      // $hasil[0] = Arr::add(['name' => 'Berbah'], 'value', Donasi::where('kecamatan','like','%Berbah%')
+      // ->count());
+      // $hasil[1] = Arr::add(['name' => 'Cangkringan'], 'value', Donasi::where('kecamatan','like','%Cangkringan%')
+      // ->count());
+      // $hasil[2] = Arr::add(['name' => 'Depok'], 'value', Donasi::where('kecamatan','like','%Depok%')
+      // ->count());
+      // $hasil[3] = Arr::add(['name' => 'Gamping'], 'value', Donasi::where('kecamatan','like','%Gamping%')
+      // ->count());
+      // $hasil[4] = Arr::add(['name' => 'Godean'], 'value', Donasi::where('kecamatan','like','%Godean%')
+      // ->count());
+      // $hasil[5] = Arr::add(['name' => 'Kalasan'], 'value', Donasi::where('kecamatan','like','%Kalasan%')
+      // ->count());
+      // $hasil[6] = Arr::add(['name' => 'Minggir'], 'value', Donasi::where('kecamatan','like','%Minggir%')
+      // ->count());
+      // $hasil[7] = Arr::add(['name' => 'Mlati'], 'value', Donasi::where('kecamatan','like','%mlati%')
+      // ->count());
+      // $hasil[8] = Arr::add(['name' => 'Moyudan'], 'value', Donasi::where('kecamatan','like','%Moyudan%')
+      // ->count());
+      // $hasil[9] = Arr::add(['name' => 'Ngaglik'], 'value', Donasi::where('kecamatan','like','%Ngaglik%')
+      // ->count());
+      // $hasil[10] = Arr::add(['name' => 'Ngemplak'], 'value', Donasi::where('kecamatan','like','%Ngemplak%')
+      // ->count());
+      // $hasil[11] = Arr::add(['name' => 'Pakem'], 'value', Donasi::where('kecamatan','like','%Pakem%')
+      // ->count());
+      // $hasil[12] = Arr::add(['name' => 'Prambanan'], 'value', Donasi::where('kecamatan','like','%Prambanan%')
+      // ->count());
+      // $hasil[13] = Arr::add(['name' => 'Seyegan'], 'value', Donasi::where('kecamatan','like','%Seyegan%')
+      // ->count());
+      // $hasil[14] = Arr::add(['name' => 'Sleman'], 'value', Donasi::where('kecamatan','like','%Sleman%')
+      // ->count());
+      // $hasil[15] = Arr::add(['name' => 'Tempel'], 'value', Donasi::where('kecamatan','like','%Tempel%')
+      // ->count());
+      // $hasil[16] = Arr::add(['name' => 'Turi'], 'value', Donasi::where('kecamatan','like','%Turi%')
+      // ->count());
 
       return $hasil;
   }
